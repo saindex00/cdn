4af02d46245740be9d6fba3282ad8d193f614533
weichien <wcvsoccer00@gmail.com>
2023-09-25 14:51:25 +0000

    alt

---
+++ sub2.js
@@ -0,0 +1,28 @@
+class CustomeStripePricingTable extends HTMLElement {
+    constructor() {
+        super();
+        // Your custom element's initialization code here
+        // You can access attributes and content inside this constructor
+        const pricingTableId = this.getAttribute('pricing-table-id');
+        const publishableKey = this.getAttribute('publishable-key');
+
+        // Create the HTML content for your custom element
+        const htmlContent = `
+        <div>
+          <h2>Stripe Pricing Table</h2>
+          <p>Pricing Table ID: ${pricingTableId}</p>
+          <p>Publishable Key: ${publishableKey}</p>
+          <!-- Add more HTML content as needed -->
+        </div>
+      `;
+
+        // Attach the HTML content to the shadow DOM of the custom element
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.innerHTML = htmlContent;
+    }
+
+    // Other methods and event handlers can be defined here
+}
+
+// Register the custom element with the browser
+customElements.define('custome-stripe-pricing-table', CustomeStripePricingTable);
